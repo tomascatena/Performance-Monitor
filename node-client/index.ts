dc22829873315c0b691beafcfd6f5a8966d4e87e
Tomás Catena <tomascatena@gmail.com>
2022-07-23 10:41:09 +0200

    send performance data to the server

--- node-client/index.ts
+++ node-client/index.ts
@@ -15,10 +15,6 @@ import { io } from 'socket.io-client';
 
 const socket = io('http://127.0.0.1:8181');
 
-socket.on('connect', () => {
-  console.log('Connected to socket.io server');
-});
-
 // CPU average
 // Get milliseconds in each mode
 const cpuAverage = () => {
@@ -65,7 +61,7 @@ const getCpuLoad = () => {
   });
 };
 
-const performanceData = () => {
+const getPerformanceData = () => {
   return new Promise(async (resolve, reject) => {
     // System information
     const osType = os.type() === 'Darwin' ? 'Mac' : os.type();
@@ -99,8 +95,49 @@ const performanceData = () => {
   });
 };
 
-performanceData().then((data) => {
+getPerformanceData().then((data) => {
   console.log(util.inspect(data, { depth: null }));
 });
 
 console.log(util.inspect(cpuAverage(), { showHidden: false, depth: null, colors: true }));
+
+socket.on('connect', () => {
+  console.log('Connected to socket.io server');
+
+  const networkInterfaces = os.networkInterfaces();
+  let macAddress = '';
+
+  for (let key in networkInterfaces) {
+    if (networkInterfaces.hasOwnProperty(key)) {
+      const networkInterface = networkInterfaces[key];
+
+      if (!networkInterface) {
+        continue;
+      }
+
+      const foundInterface = networkInterface.find(
+        (nI) => nI.family === 'IPv4' && nI.internal === false
+      );
+
+      if (foundInterface) {
+        macAddress = foundInterface.mac;
+        break;
+      }
+    }
+  }
+
+  // Client auth with single key value
+  socket.emit('client-auth', 'pelusa');
+
+  // Send performance data to server every second
+  let performanceDataInterval = setInterval(async () => {
+    const performanceData = await getPerformanceData();
+
+    socket.emit('performance-data', performanceData);
+  }, 1000);
+
+  socket.on('disconnect', () => {
+    // If the server restarts or the client reconnect to the server, the interval will be cleared
+    clearInterval(performanceDataInterval);
+  });
+});
--- server/socket-handlers.ts
+++ server/socket-handlers.ts
@@ -1,5 +1,35 @@
 import { Server, Socket } from 'socket.io';
 
+type PerformanceData = {
+  osType: string;
+  uptime: number;
+  freeMemory: number;
+  totalMemory: number;
+  usedMemory: number;
+  memoryUsage: number;
+  numberOfCores: number;
+  cpuModel: string;
+  cpuSpeed: number;
+  cpuLoad: number;
+};
+
 export const socketMain = (io: Server, socket: Socket) => {
   console.log('Someone connected', socket.id);
+
+  socket.on('client-auth', (key) => {
+    if (key === 'pelusa') {
+      console.log('Valid client joined');
+      socket.join('clients');
+    } else if (key === 'pelusa2') {
+      console.log('Valid UI client has joined');
+      socket.join('ui');
+    } else {
+      console.log('Invalid client joined');
+      socket.disconnect(true); // terminate the underlying connection
+    }
+  });
+
+  socket.on('performance-data', (data: PerformanceData) => {
+    console.log('Received performance data', data);
+  });
 };
